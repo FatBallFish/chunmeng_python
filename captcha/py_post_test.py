b526af492bc277f6f5c447694d3c766c2a0e5a9c
FatBallFish <893721708@qq.com>
2019-03-06 02:10:01 +0800

    update

--- captcha/py_post_test.py
+++ captcha/py_post_test.py
@@ -4,7 +4,7 @@ import base64
 headers = {'content-type': "application/json"}
 data={"id":0,"status":0,"type":"img","subtype":"generate","data":{}}
 response = requests.post(url="http://127.0.0.1:8080/captcha",data=json.dumps(data),headers=headers)
-print(response.text)
+print(response.json())
 # data_json = response.json()
 # data = data_json["data"]
 # img_b64 = data["imgdata"]
